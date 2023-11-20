2c30d1c1f7da21f3b4ef7ae9bb6ec89e6d323186
Michele Ursino <michele@micurs.com>
2023-11-20 11:24:39 -0800

    Improve demo page

--- demos/demo-01/src/main.ts
+++ demos/demo-01/src/main.ts
@@ -1,30 +1,34 @@
+import { Gpu } from '@shaders-mono/webgpu';
 import { init } from './model-builder';
 
-const supportEl = document.getElementById('support') as HTMLParagraphElement | null;
-const canvasEl = document.getElementById('gfx-canvas') as HTMLCanvasElement | null;
+const getOnClickHandler = (gpu: Gpu, checkbox: HTMLInputElement) => {
+  return () => {
+    console.log('setWireframe', checkbox.checked);
+    if (checkbox.checked) {
+      gpu.setPipelineMode('alternative');
+    } else {
+      gpu.setPipelineMode('default');
+    }
+  };
+};
+
 const image = new Image();
 image.src = 'earth.jpg';
 image.onload = () => {
+  const supportEl = document.getElementById('support') as HTMLParagraphElement | null;
+  const canvasEl = document.getElementById('gfx-canvas') as HTMLCanvasElement | null;
   if (!supportEl || !canvasEl) {
     alert('The app is broken! No canvas was found!');
-  } else {
-    createImageBitmap(image)
-      .then((texture) => init(canvasEl, supportEl, texture))
-      .then((gpu) => {
-        supportEl!.innerText = 'All set!';
-
-        (globalThis as any).setWireframe = () => {
-          const checkbox = document.getElementById('wireframe') as HTMLInputElement;
-          console.log('setWireframe', checkbox.checked);
-          if (checkbox.checked) {
-            gpu.setPipelineMode('alternative');
-          } else {
-            gpu.setPipelineMode('default');
-          }
-        };
-      })
-      .catch((err) => {
-        supportEl!.innerText = 'Error: ' + err.message;
-      });
+    return;
   }
+  createImageBitmap(image)
+    .then((texture) => init(canvasEl, supportEl, texture))
+    .then((gpu) => {
+      const checkbox = document.getElementById('wireframe') as HTMLInputElement;
+      checkbox.onclick = getOnClickHandler(gpu, checkbox);
+      supportEl!.innerText = 'All set!';
+    })
+    .catch((err) => {
+      supportEl!.innerText = 'Error: ' + err.message;
+    });
 };
